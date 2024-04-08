09613a383d8761407f52f20a84b7ac207e52b120
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-08 18:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+let table = document.querySelector("table");
+let tbody = document.createElement("tbody");
+let tr = document.createElement("tr");
+tr.innerHTML = `<td colspan="2">Loading...</td>`;
+tbody.append(tr);
+table.append(tbody);
+
+let promise1 = new Promise((resolve,reject)=>{
+	const startTime = Date.now(); 
+	setTimeout(()=>{
+		const endTime = Date.now(); 
+      const timeTaken = (endTime - startTime) / 1000; 
+		resolve(timeTaken)
+	},1000)
+});
+
+let promise2 = new Promise((resolve,reject)=>{
+	const startTime = Date.now(); 
+	setTimeout(()=>{
+		const endTime = Date.now(); 
+      const timeTaken = (endTime - startTime) / 1000; 
+		resolve(timeTaken)
+	},2000)
+});
+
+let promise3 = new Promise((resolve,reject)=>{
+	const startTime = Date.now(); 
+	setTimeout(()=>{
+		const endTime = Date.now(); 
+      const timeTaken = (endTime - startTime) / 1000; 
+		resolve(timeTaken)
+	},3000)
+});
+
+let result = Promise.all([promise1,promise2,promise3]);
+result.then((resolvedPromises)=>{
+	tbody.innerHTML = "";
+	resolvedPromises.forEach((element,index)=>{
+		let tr = document.createElement("tr");
+tr.innerHTML = `<td>Promise ${index+1}</td>
+<td>${parseInt(element)}</td>
+`;
+		tbody.append(tr);
+	})
+})
+console.log(result);
+
+
+
+
+
